f51510e672f4cb10f84c27814196f9556d5f5f4d
davidk <kdavid@gmail.com>
2023-05-05 05:40:54 +0000

    builder.sh: Change appimage tooling architecture when we are not x86_64

--- builder.sh
+++ builder.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+DPKG_ARCH="$(dpkg --print-architecture)"
+
+if [[ "${DPKG_ARCH}" == "armhf" ]]; then
+  APPIMAGE_ARCH="armhf"
+elif [[ "${DPKG_ARCH}" == "arm64" ]]; then
+  APPIMAGE_ARCH="aarch64"
+else
+  echo "Unknown architecture [arch: ${DPKG_ARCH}]."
+  echo "Please update the build assistant to add support."
+  exit 1
+fi
+
 podman build -t superslicer-builder .
 
 if [[ ! -d "superslicer" ]]; then
@@ -7,6 +19,7 @@ if [[ ! -d "superslicer" ]]; then
 fi
 
 cd superslicer
-podman run -v $PWD:/superslicer:z -it superslicer-builder ./BuildLinux.sh -u && ./BuildLinux.sh -ds
+podman run -v $PWD:/superslicer:z -it superslicer-builder ./BuildLinux.sh -u && ./BuildLinux.sh -ds 
 
-# todo: generate appimage, arch dependent
+sed -ie "s@x86_64@${APPIMAGE_ARCH}" build/build_appimage.sh 
+podman run -v $PWD:/superslicer:z -it superslicer-builder ./BuildLinux.sh -u && ./BuildLinux.sh -i
